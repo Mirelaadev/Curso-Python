fefbf95e5d7c18c6c276428fd0418a5d71a01b07
Mirela <santosmirela402@gmail.com>
2026-05-03 19:23:38 -0300

    fix: melhora lógica e validação do ex01

--- Aula01/ex01.py
+++ Aula01/ex01.py
@@ -1,8 +1,13 @@
 inicio = int(input("Digite o inicio do intervalo: "))
 fim = int(input("Digite o fim do intervalo: "))
 
-for x in range(inicio, fim +1, 2):
-    if x % 2 == 0:
-        print(x)
-    else:
-        print(x+1)
+if inicio < fim:
+    for x in range(inicio, fim):
+        if x % 2 == 0:
+            print(x)
+elif inicio > fim:
+    for x in range(fim, inicio):
+        if x % 2 == 0:
+            print(x)
+else:
+    print("Valores iguais")
