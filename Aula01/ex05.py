c5422d4c7c26fa560591c11f608ae1c8a8957a66
Mirela <santosmirela402@gmail.com>
2026-05-03 22:40:39 -0300

    fix: melhora lógica e validação

--- Aula01/ex05.py
+++ Aula01/ex05.py
@@ -1,5 +1,8 @@
 print("Tabuada")
 numero = int(input("Digite o número da tabuada: "))
 
-for x in range(1, 11):
-    print(numero, "X", x, "=", (x * numero))
+if numero > 0 and numero <= 10:
+    for x in range(1, 11):
+        print(numero, "X", x, "=", (x * numero))
+else:
+    print("Número invalido")
