991417a2179bc6ea706bb51d67f664aae460af37
Mirela <santosmirela402@gmail.com>
2026-05-12 16:53:30 -0300

    refactor: melhora estrutura do código

--- Aula02/ex03.py
+++ Aula02/ex03.py
@@ -1,37 +1,40 @@
 class Numero:
-    def __init__(self, n1, n2, n3):
+    n1 = 0
+    n2 = 0
+    n3 = 0
+    maior = 0
+    menor = 0
+
+    def receber_numeros(self, n1, n2, n3):
         self.n1 = n1
         self.n2 = n2
         self.n3 = n3
 
     def calcular(self):
-        maior = self.n1
-        menor = self.n1
+        self.maior = self.n1
+        self.menor = self.n1
 
-        if(self.n1 == self.n2 or self.n2 == self.n3 or self.n3 == self.n1):
-            return "Os numeros são iguais!"
+        if self.n2 > self.maior:
+            self.maior = self.n2
 
-        if self.n2 > maior:
-            maior = self.n2
+        if self.n3 > self.maior:
+            self.maior = self.n3
 
-        if self.n3 > maior:
-            maior = self.n3
+        if self.n2 < self.menor:
+            self.menor = self.n2
 
-        if self.n2 < menor:
-            menor = self.n2
-
-        if self.n3 < menor:
-            menor = self.n3
-
-        print(f"Maior número: {maior} - Menor número: {menor}")
+        if self.n3 < self.menor:
+            self.menor = self.n3
 
     def exibir_resultado(self):
-        return self.calcular()
+        print("Maior número: ", self.maior)
+        print("Menor número: ", self.menor)
 
-
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite um número: "))
-n3 = int(input("Digite um número: "))
-
-numero = Numero(n1, n2, n3)
+numero = Numero()
+numero.receber_numeros(
+    float(input("Digite o primeiro número: ")),
+    float(input("Digite o segundo número: ")),
+    float(input("Digite o terceiro número: "))
+)
+numero.calcular()
 numero.exibir_resultado()
