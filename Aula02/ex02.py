c25315aedb94ed06acd53cbd7c98eeaf222b0137
Mirela <santosmirela402@gmail.com>
2026-05-12 16:31:19 -0300

    refactor: melhora estrutura do código

--- Aula02/ex02.py
+++ Aula02/ex02.py
@@ -1,17 +1,20 @@
 class Download:
-    def __init__(self, tamArquivo, velInternet):
-        self.tamArquivo = tamArquivo
-        self.velInternet = velInternet
+    tamArquivo = 0 #MB
+    velInternet = 0 #Mbps
+    tempAprox = 0
 
-    def tempAprox(self):
-        tempoMin = (self.tamArquivo / (self.velInternet / 8) ) /60
-        return tempoMin
+    def entrada_dados(self):
+        self.tamArquivo = float(input("Informe o tamanho do arquivo (MB): "))
+        self.velInternet = float(input("Informe a velocidade do internet: "))
+
+    def calcular(self):
+        self.velInternet = self.velInternet / 8 #transformando bit em byte
+        self.tempAprox = (self.tamArquivo / self.velInternet) / 60
 
     def exibir_resultado(self):
-        print("Tempo aproximado de download: {:.2f} minutos".format(self.tempAprox()))
+        print("Tempo aproximado de download em minutos é de ", self.tempAprox)
 
-tamanho = float(input("Informe o tamanho do arquivo (MB): "))
-velocidade = float(input("Informe a velocidade da internet (Mbps): "))
-
-download = Download(tamanho, velocidade)
+download = Download()
+download.entrada_dados()
+download.calcular()
 download.exibir_resultado()
